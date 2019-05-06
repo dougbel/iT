8a47149ee93719f4e2cefb3e5b2d82484bdaa025
Abel Pacheco-Ortega <dougbel.unam@gmail.com>
2019-05-06 01:45:27 +0100

    First draft for Provenance vector calculation

---
+++ include/provenancevectors_it.h
@@ -0,0 +1,38 @@
+#ifndef PROVENANCEVECTORS_IT_H
+#define PROVENANCEVECTORS_IT_H
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/filters/extract_indices.h>
+#include "type_point_it.h"
+
+class ProvenanceVectors_iT
+ {
+    public:
+        
+        pcl::PointCloud<pcl::PointXYZ>::Ptr ibs;
+        
+        pcl::PointCloud<pcl::PointXYZ>::Ptr sceneCloud;
+        
+        pcl::PointCloud<pcl::PointNormal>::Ptr  rawProvenanceVectors;
+        
+        pcl::PointCloud<pcl::PointNormal>::Ptr  smoothedProvenanceVectors;
+        
+        float   maxV;
+        float   minV;
+        
+        
+        ProvenanceVectors_iT( pcl::PointCloud<pcl::PointXYZ>::Ptr ibs, pcl::PointCloud<pcl::PointXYZ>::Ptr scene_cloud );
+        
+        void calculateProvenanceVectors(int knnToSmooth);
+        
+        
+       
+};
+
+
+
+
+#endif // PROVENANCEVECTORS_IT_H
